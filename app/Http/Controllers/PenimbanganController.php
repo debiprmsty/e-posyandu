8fb6673db16f407a9c34ea7f78b4364fab256375
Luh Debi Pramesty <debipramesty@gmail.com>
2023-08-21 22:40:19 +0800

    final-eposyandu

--- app/Http/Controllers/PenimbanganController.php
+++ app/Http/Controllers/PenimbanganController.php
@@ -19,7 +19,7 @@ class PenimbanganController extends Controller
     public function index()
     {
         $data = Penimbangan::with(['dusun', 'balita', 'keterangan'])->get();
-        $dataDusun = Dusun::all();
+        $dataDusun = Dusun::has('balita')->with('balita')->get();
         return view('penimbangan', compact('data', 'dataDusun'));
     }
 
